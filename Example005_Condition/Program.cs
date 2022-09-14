a5758522546e9d948d6a035c7da28ee619e282e2
Robert <IlyasovRV@gmai.comt>
2022-09-14 15:03:16 +0500

    Example005 Condition

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "роберт")
+{
+    Console.WriteLine("Ура, это же Роберто!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Сonsole.WriteLine(username);
+}
